e718fd75f8f659432bb35bacc92e4df4889801d5
nunesdelfino <gabrielnunesdelfinoo@outlook.com>
2020-06-26 13:08:51 -0300

    IP Externo via Wget

--- MeuIPWget.sh
+++ MeuIPWget.sh
@@ -2,7 +2,7 @@
 
 ARQUIVO="MeuIP.html"
 
-wget https://www.meuip.com.br/ -O ARQUIVO
+wget -q https://www.meuip.com.br/ -O ARQUIVO
 
 cat ARQUIVO | grep "Meu ip é" | sed 's/<[^>]*>//g' | sed 's/	.*	//g' | sed 's/Meu.* /Meu IP Publico: /g'
 
