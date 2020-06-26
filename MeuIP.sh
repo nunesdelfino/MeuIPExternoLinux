fe29c57dea627349171dd9a7289181710ece6541
Gabriel Delfino <47621308+nunesdelfino@users.noreply.github.com>
2020-06-26 12:47:16 -0300

    Update MeuIP.sh

--- MeuIP.sh
+++ MeuIP.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl https://www.meuip.com.br/ | grep Meu ip é | sed 's/<[^>]*>//g' | sed 's/	.*	//g' | sed 's/Meu.* /Meu IP Publico: /g'
+curl https://www.meuip.com.br/ | grep "Meu ip é" | sed 's/<[^>]*>//g' | sed 's/	.*	//g' | sed 's/Meu.* /Meu IP Publico: /g'
